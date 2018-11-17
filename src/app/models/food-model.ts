a264f729ce60fd556eeef7f1122e97f270a40df6
Thomas Hill <thomashill1993@me.com>
2018-11-17 12:20:07 -0600

    menu goes through database

--- src/app/models/food-model.ts
+++ src/app/models/food-model.ts
@@ -5,10 +5,13 @@ export class FoodModel
     public name:string;
     public price: number;
     public category:FOOD_CATEGORY;
-    constructor(name:string, price:number, category:FOOD_CATEGORY)
+    public description:string;
+
+    constructor(name:string, price:number, category:FOOD_CATEGORY, description = 'description')
     {
         this.name = name;
         this.price = price;
         this.category = category;
+        this.description = description;
     }
 }
