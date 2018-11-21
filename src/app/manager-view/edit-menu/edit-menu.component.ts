91448503616d7d91371a9c81d8ede8840041fe07
Afolabi <aige1@lsu.edu>
2018-11-20 21:13:06 -0600

    Added edit menu

--- src/app/manager-view/edit-menu/edit-menu.component.ts
+++ src/app/manager-view/edit-menu/edit-menu.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from 'src/app/services/menu.service';
+import {MenuModel} from '../../models/menu-model';
+import { TableModel } from 'src/app/models/table-model';
+import {FoodModel, FOOD_CATEGORY} from '../../models/food-model';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-edit-menu',
@@ -7,9 +13,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditMenuComponent implements OnInit {
 
-  constructor() { }
+  table: TableModel;
+  menuApps: FoodModel[] = [];
+  menuSides:FoodModel[] = [];
+  menuEntrees:FoodModel[]=[];
+  menuDesserts:FoodModel[]=[];
+  menu: MenuModel;
+  constructor(private _menuService: MenuService, private modalService: NgbModal) { 
+  }
 
-  ngOnInit() {
+  addFoodToPending(food:FoodModel){
+    this.table.addToPending(food);
+  }
+
+  ngOnInit( ) {
+    this._menuService.getOrderObservable().subscribe((menuItems:FoodModel[])=>{
+      this.menuApps = menuItems.filter(f => f.category == FOOD_CATEGORY.APPETIZER);
+      this.menuSides = menuItems.filter(f => f.category == FOOD_CATEGORY.SIDE);
+      this.menuEntrees = menuItems.filter(f => f.category == FOOD_CATEGORY.ENTREE);
+      this.menuDesserts = menuItems.filter(f => f.category == FOOD_CATEGORY.DESSERT);
+  });
+  }
+
+  closeResult: string;
+
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
 }
