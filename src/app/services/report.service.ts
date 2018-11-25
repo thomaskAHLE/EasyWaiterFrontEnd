3012a24141bc71e095f891dbaefc632d278f091f
Thomas Hill <thomashill1993@me.com>
2018-11-25 17:59:56 -0600

    create report functions working

--- src/app/services/report.service.ts
+++ src/app/services/report.service.ts
@@ -4,27 +4,70 @@ import { OrderModel,ORDER_STATUS } from '../models/order-model';
 import { FoodModel } from '../models/food-model'; 
 import { Observable } from 'rxjs';
 import {map} from 'rxjs/operators';
+import { UserService } from './user.service';
+import { UserModel } from '../models/user-model';
+import { MenuService } from './menu.service';
+import { ReportModel } from '../models/report-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportService {
 
-  ordersCollection: AngularFirestoreCollection<OrderModel>; 
-  orders: Observable<OrderModel[]>; 
-  constructor(private _afs:AngularFirestore) { 
+  ordersCollection: AngularFirestoreCollection<ReportModel>; 
+  orders: Observable<ReportModel[]>; 
+  allUsers: UserModel[];
+  menuItems:FoodModel[];
+  salesReportMap: Map<string,Number>;
+  employeeReportMap:Map<string,Number>;
+  constructor(private _afs:AngularFirestore, private userService:UserService, private menuService: MenuService) { 
     this.ordersCollection = this._afs.collection('deliveredOrders'); 
-    this.orders = this.ordersCollection.valueChanges(); 
-    //, ref => {
-      //return ref.orderBy('food', 'desc')
-    //});
-    
+    this.userService.getUsers().subscribe((users:UserModel[]) => this.allUsers = users);
+    this.menuService.getMenuObservable().subscribe((foods:FoodModel[])=> this.menuItems = foods);
   } 
 
 
   generateSalesReport()
-  {
-    
+  { 
+    this.salesReportMap = new Map<string, Number>();
+    this.employeeReportMap = new Map<string, Number>();
+    this.salesReportMap.clear();
+    this.employeeReportMap.clear();
+    this.orders= this.ordersCollection.snapshotChanges().pipe(map(report=> report.map(r=>
+      {const data = new ReportModel(r.payload.doc.data().employee, r.payload.doc.data().name, r.payload.doc.data().price, r.payload.doc.id); return data})));
+    this.orders.subscribe((reports:ReportModel[]) => {
+      console.log(reports);
+        reports.forEach(r =>
+          {
+            if(this.employeeReportMap.has(r.employee))
+            {
+              this.employeeReportMap[r.employee] =  Number(this.employeeReportMap.get(r.employee))+ Number(r.price);
+              console.log(Number(r.price));
+            }
+            else
+            {
+              this.employeeReportMap.set(r.employee, Number(r.price));
+              console.log(Number(r.price));
+            }
+            if(this.salesReportMap.has(r.name))
+            {
+              this.salesReportMap[r.name] = Number(this.salesReportMap.get(r.name)) + Number(r.price);
+              console.log(Number(r.price));
+            }
+            else
+            {
+              this.salesReportMap.set(r.name, Number(r.price));
+              console.log(Number(r.price));
+            }
+          })
+
+    console.log("testing salesReport map");
+    console.log(this.salesReportMap);
+
+    console.log("testing employeeReportMap");
+    console.log(this.employeeReportMap);
+    })
+
     //var allOrders = delivered.
     
     //.forEach(doc => {
--- src/app/services/restaurant.service.ts
+++ src/app/services/restaurant.service.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@angular/core';
 import { TableModel } from '../models/table-model';
 import { UserModel, USER_TYPE } from '../models/user-model';
 import { UserService } from './user.service';
-import { OrderModel,ORDER_STATUS } from '../models/order-model';
-import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from '@angular/fire/firestore';
+import { OrderModel, ORDER_STATUS } from '../models/order-model';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { ReportModel } from '../models/report-model';
 @Injectable({
   providedIn: 'root'
@@ -16,71 +16,74 @@ export class RestaurantService {
   tables: TableModel[];
   orderCollection: AngularFirestoreCollection<OrderModel>;
   waiters: UserModel[];
-  path:string = "/orders";
-  constructor( private afs:AngularFirestore) {
+  path: string = "/orders";
+  constructor(private afs: AngularFirestore) {
     this.orderCollection = this.afs.collection('orders');
   }
 
-  
- 
-  getToDoOrders():Observable<OrderModel[]>{
-    let v:AngularFirestoreCollection<OrderModel> = this.afs.collection('orders', ref =>{
-      return ref.where('status','==',ORDER_STATUS.TO_DO) 
+
+
+  getToDoOrders(): Observable<OrderModel[]> {
+    let v: AngularFirestoreCollection<OrderModel> = this.afs.collection('orders', ref => {
+      return ref.where('status', '==', ORDER_STATUS.TO_DO)
     });
-    return v.snapshotChanges().pipe(map(obj => obj.map(o => {const data = new OrderModel(o.payload.doc.data().food,o.payload.doc.data().tableNumber, o.payload.doc.data().status); data.$key = o.payload.doc.id; return data;})));
+    return v.snapshotChanges().pipe(map(obj => obj.map(o => { const data = new OrderModel(o.payload.doc.data().food, o.payload.doc.data().tableNumber, o.payload.doc.data().status); data.$key = o.payload.doc.id; return data; })));
   }
 
-  addPendingToOrders(ordersToAdd: OrderModel[]):void
-  {
+  addPendingToOrders(ordersToAdd: OrderModel[]): void {
     console.log('adding pending to orders');
-    ordersToAdd.forEach(o => console.log(JSON.parse(JSON.stringify(o)))); 
-    ordersToAdd.forEach( o => this.orderCollection.add(JSON.parse(JSON.stringify(o))));
+    ordersToAdd.forEach(o => console.log(JSON.parse(JSON.stringify(o))));
+    ordersToAdd.forEach(o => this.orderCollection.add(JSON.parse(JSON.stringify(o))));
   }
 
-  
 
 
-  getInProgOrders():Observable<OrderModel[]>{
-    let v:AngularFirestoreCollection<OrderModel> = this.afs.collection('orders', ref =>{
-      return ref.where('status','==',ORDER_STATUS.IN_PROGRESS) 
+
+  getInProgOrders(): Observable<OrderModel[]> {
+    let v: AngularFirestoreCollection<OrderModel> = this.afs.collection('orders', ref => {
+      return ref.where('status', '==', ORDER_STATUS.IN_PROGRESS)
     });
-    return v.snapshotChanges().pipe(map(obj => obj.map(o => {const data = new OrderModel(o.payload.doc.data().food,o.payload.doc.data().tableNumber, o.payload.doc.data().status); data.$key = o.payload.doc.id; return data;})));
+    return v.snapshotChanges().pipe(map(obj => obj.map(o => { const data = new OrderModel(o.payload.doc.data().food, o.payload.doc.data().tableNumber, o.payload.doc.data().status); data.$key = o.payload.doc.id; return data; })));
   }
-  
-  getFinishedOrders():Observable<OrderModel[]>{
-    let v:AngularFirestoreCollection<OrderModel> = this.afs.collection('orders', ref =>{
-      return ref.where('status','==',ORDER_STATUS.FINISHED) 
+
+  getFinishedOrders(): Observable<OrderModel[]> {
+    let v: AngularFirestoreCollection<OrderModel> = this.afs.collection('orders', ref => {
+      return ref.where('status', '==', ORDER_STATUS.FINISHED)
     });
-    return v.snapshotChanges().pipe(map(obj => obj.map(o => {const data = new OrderModel(o.payload.doc.data().food,o.payload.doc.data().tableNumber, o.payload.doc.data().status); data.$key = o.payload.doc.id; return data;})));
+    return v.snapshotChanges().pipe(map(obj => obj.map(o => { const data = new OrderModel(o.payload.doc.data().food, o.payload.doc.data().tableNumber, o.payload.doc.data().status); data.$key = o.payload.doc.id; return data; })));
   }
-  
-  getOrderObservable(): Observable<OrderModel[]>{
-    return this.orderCollection.snapshotChanges().pipe(map(obj => obj.map(o => {const data = new OrderModel(o.payload.doc.data().food,o.payload.doc.data().tableNumber, o.payload.doc.data().status);  data.$key = o.payload.doc.id; return data;}
+
+  getOrderObservable(): Observable<OrderModel[]> {
+    return this.orderCollection.snapshotChanges().pipe(map(obj => obj.map(o => { const data = new OrderModel(o.payload.doc.data().food, o.payload.doc.data().tableNumber, o.payload.doc.data().status); data.$key = o.payload.doc.id; return data; }
     )));
   }
 
-  updateOrderStatus(order:OrderModel):void
-  {
-    this.orderCollection.doc(order.$key).update({status: order.status});
+  updateOrderStatus(order: OrderModel): void {
+    this.orderCollection.doc(order.$key).update({ status: order.status });
   }
-  getOrderObservableforTable(tableNum:number):Observable<OrderModel[]>{
-   let v:AngularFirestoreCollection<OrderModel> = this.afs.collection('orders', ref =>{
-      return ref.where('tableNumber','==',tableNum.toString()) 
+  getOrderObservableforTable(tableNum: number): Observable<OrderModel[]> {
+    let v: AngularFirestoreCollection<OrderModel> = this.afs.collection('orders', ref => {
+      return ref.where('tableNumber', '==', tableNum.toString())
     });
-    return v.snapshotChanges().pipe(map(obj => obj.map(o => {const data = new OrderModel(o.payload.doc.data().food,o.payload.doc.data().tableNumber, o.payload.doc.data().status); data.$key = o.payload.doc.id; 
-    return data;})));
+    return v.snapshotChanges().pipe(map(obj => obj.map(o => {
+      const data = new OrderModel(o.payload.doc.data().food, o.payload.doc.data().tableNumber, o.payload.doc.data().status); data.$key = o.payload.doc.id;
+      return data;
+    })));
   }
 
-  clearTablesOrders(table:TableModel ):void{
-    let deliveredOrdersCollection:AngularFirestoreCollection<OrderModel> = this.afs.collection('deliveredOrders');
-    this.getOrderObservableforTable(table.tableNumber).subscribe((orders:OrderModel[])=>{
-      orders.forEach(o=> {
-      let reportItem: ReportModel = new ReportModel(table.assignedTo, o.food.name, o.food.price);
-      deliveredOrdersCollection.add(JSON.parse(JSON.stringify(reportItem)))}); 
-      orders.forEach(o =>{let orderDocument:AngularFirestoreDocument<OrderModel> = this.afs.doc<OrderModel>(`orders/${o.$key}`);
-      orderDocument.delete();
-    })
-    })
+  clearTablesOrders(table: TableModel): void {
+    let deliveredOrdersCollection: AngularFirestoreCollection<ReportModel> = this.afs.collection('deliveredOrders');
+    this.getOrderObservableforTable(table.tableNumber).subscribe((orders: OrderModel[]) => {
+      orders.forEach(o => {
+        const order = {employee: table.assignedTo, name:o.food.name, price: o.food.price.toString(), key:null}
+        console.log(order)
+        deliveredOrdersCollection.add(order);
+        orders.forEach(o => {
+          let orderDocument: AngularFirestoreDocument<OrderModel> = this.afs.doc<OrderModel>(`orders/${o.$key}`);
+          orderDocument.delete();
+        })
+      })
 
+    })
   }
 }
