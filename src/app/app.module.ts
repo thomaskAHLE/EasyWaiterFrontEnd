a6091dae219f64dd09a1b25fafb953990befc45e
Afolabi <aige1@lsu.edu>
2018-12-03 08:58:35 -0600

    Cleaned up and pushed final changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,12 +26,8 @@ import { ModalComponent } from './manager-view/modal/modal.component';
 import { EMmodalComponent } from './manager-view/emmodal/emmodal.component';
 import { ClrModalComponent } from './waiter-view/clr-modal/clr-modal.component';
 import { BackModalComponent } from './waiter-view/back-modal/back-modal.component';
-<<<<<<< HEAD
-import { RmItemModalComponent } from './manager-view/rm-item-modal/rm-item-modal.component';
-
-=======
 import {RmItemModalComponent} from './manager-view/rm-item-modal/rm-item-modal.component'
->>>>>>> 7fa876cbc72b6263ac389aabca396213c5a3b4ed
+
 
 const appRoute: Routes = [
   {path: 'waiter-view', component: WaiterViewComponent},
@@ -60,13 +56,8 @@ const appRoute: Routes = [
     ModalComponent,
     EMmodalComponent,
     ClrModalComponent,
-<<<<<<< HEAD
-    BackModalComponent,
-    RmItemModalComponent
-=======
     RmItemModalComponent,
     BackModalComponent
->>>>>>> 7fa876cbc72b6263ac389aabca396213c5a3b4ed
   ],
   imports: [
     BrowserModule,
@@ -89,14 +80,9 @@ const appRoute: Routes = [
     ModalComponent,
     EMmodalComponent,
     ClrModalComponent,
-<<<<<<< HEAD
-    BackModalComponent,
-    RmItemModalComponent],
-=======
     RmItemModalComponent,
     BackModalComponent],
 
->>>>>>> 7fa876cbc72b6263ac389aabca396213c5a3b4ed
   bootstrap: [AppComponent]
 })
 export class AppModule { }
